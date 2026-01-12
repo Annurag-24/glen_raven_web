74e8d53be4ff4032ba7b47fe55783d65091d078f
Rohan Daddekar <rohandaddekar9@gmail.com>
2026-01-12 15:39:04 +0530

    feat: add Header and LeftSidebar components with navigation functionality
    - Created Header component for the top navigation bar, including user icon and dropdown.
    - Developed LeftSidebar component to display menu and quick action items with routing.
    - Introduced navigation constants for menu and action items with associated icons and paths.

--- src/layouts/Dashboard.tsx
+++ src/layouts/Dashboard.tsx
@@ -1,10 +1,22 @@
 import { Outlet } from "react-router";
+import Header from "@/components/Header";
+import LeftSidebar from "@/components/LeftSidebar";
 
 const DashboardLayout = () => {
   return (
-    <>
-      <Outlet />
-    </>
+    <div className="h-screen flex flex-col">
+      <Header />
+
+      <div className="flex flex-1 overflow-hidden">
+        <div className="overflow-y-auto [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none]">
+          <LeftSidebar />
+        </div>
+
+        <main className="flex-1 overflow-y-auto p-4 bg-[#F9FAFB]">
+          <Outlet />
+        </main>
+      </div>
+    </div>
   );
 };
 
